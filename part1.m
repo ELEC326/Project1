3ae914ed2a4dd631d4da703960d9af914019b80d
kellanrankin <14kr2@queensu.ca>
2016-10-30 13:59:31 -0400

    Update part1.m

--- part1.m
+++ part1.m
@@ -1,4 +1,4 @@
-%%this is Matlab Code for Part 1
+%%this is Matlab Code for Question 1
 close all
 clear all
 clc
@@ -7,9 +7,9 @@ clc
 X = linspace(1,20,20);
 Y = linspace(3,18,16);
 i=1;
-for k= 1:1:20
+for k = 1:1:20
     if i<7
-        if k==X(i)
+        if (k==X(i))
             PX(k)=1/6;
             i=i+1;
         else
@@ -21,14 +21,14 @@ for k= 1:1:20
 end
 
 PY=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    for i= 1:1:20
-        for k= 1:1:6
-            for l=1:1:6
-                for m= 1:1:6
-                    if i==k+l+m
+    for i = 1:1:20
+        for k = 1:1:6
+            for l = 1:1:6
+                for m = 1:1:6
+                    if i == k + l + m
                         PY(i)= PY(i)+PX(k)*PX(l)*PX(m);
-                        test=i;
-                        testP=PY(i);
+                        test = i;
+                        testP = PY(i);
                     end
                 end
            end
@@ -36,17 +36,17 @@ PY=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     end
 
 subplot(1,2,1)
-stem(PX)
+stem(PX,'g')
 hold on;
 stem (PY)
 hold on;
 xlabel('Value')
 ylabel('Probability')
+legend('PMF X', 'PMF Y')
 title('PMF of X and Y')
 
 %%
 %-----------------------part II-----------------------------------%
-%FX(k)=SUM[PX(xmin)to PX(k)]
 FX=[0];
 FY=[0];
 FX(1)=PX(1);
@@ -57,22 +57,67 @@ for k= 2:1:20
 end
 
 subplot(1,2,2)
-stem(FX)
+stem(FX,'r')
 hold on;
 stem(FY)
 hold on;
 xlabel('Value')
 ylabel('Probability')
+legend('CDF X', 'CDF Y')
 title('CDF of X and Y')
 
-%%
-%-----------------------part III-----------------------------------%
-for n = 1:100
+ %%
+%-----------------------part III -----------------------------------%
+H1 = zeros(6,1);
+H2 = zeros(6,1);
+H3 = zeros(6,1);
+HY = zeros(18,1);
+
+N = 100;
+for n = 1:N
     x1(n) = randi(6);
     x2(n) = randi(6);
     x3(n) = randi(6);
-    y(n) = x1(n)+x2(n)+x3(n)
+    y(n) = x1(n) + x2(n) + x3(n);
 end
-
 %%
-%------------------------part IV----------------------------------%
+%-----------------------part IV -----------------------------------%
+%loop to see occurances of each number
+for i = 1:N
+    for k = 1:6
+        if (x1(i) == k)
+            H1(k) = H1(k) + 1;
+        end
+        if (x2(i) == k)
+            H2(k) = H2(k) + 1;
+        end 
+        if (x3(i) == k)
+            H3(k) = H3(k) + 1;
+        end
+    end
+      
+ %-----------------------part V -----------------------------------%
+   %loop to check occurances of sums   
+        for k = 3:18
+            if (y(i) == k)
+                HY(k) = HY(k) + 1;
+            end
+        end
+ end
+  
+    
+    
+    HTOTAL = [H1,H2,H3];
+    figure(3);
+    bar(HTOTAL);
+    legend('Dice 1', 'Dice 2', 'Dice 3', 'location', 'southeast')
+    xlabel('Value')
+    ylabel('Probability')
+    title('Occuraances of each outcome')
+
+
+    figure(4)
+    bar(HY)
+    xlabel('Value')
+    ylabel('Probability')
+    title('Occurances of summed outcomes')
